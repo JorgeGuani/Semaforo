d77b355772bc75ef787014d167074bd6e890cab4
Jorge Guani <jorge_guani@hotmail.com>
2018-09-11 09:14:14 -0500

    Commit inicial

---
+++ Semaforov2.ino
@@ -0,0 +1,72 @@
+#define pinBoton 2
+#define pinLedVerde 3
+#define pinLedAmarillo 4
+#define pinLedRojo 5
+#define pinLedVerdeP 10
+#define pinLedRojoP 11
+#define pot 1
+const int tiempoLedVerde = 5000;
+const int tiempoLedAmarillo = 2500;
+const int tiempoLedRojo = 5000;
+const int tiempoParpadeo = 600;
+boolean flag = false;
+float contador = 0;
+int valorPot = 0;
+
+void setup() {
+  pinMode(pinBoton, INPUT_PULLUP);
+  pinMode(pinLedVerde, OUTPUT);
+  pinMode(pinLedAmarillo, OUTPUT);
+  pinMode(pinLedRojo, OUTPUT);
+  pinMode(pinLedVerdeP, OUTPUT);
+  pinMode(pinLedRojoP, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() { 
+  contador += 0.157;
+  valorPot = analogRead(pot)*20;
+  
+  if(digitalRead(pinBoton) == HIGH) {
+    flag = true;
+  }else {
+    flag = false;
+  }
+  if(flag == true) { //semaforo normal
+    apagarLeds();
+    digitalWrite(pinLedVerde, HIGH);    
+    digitalWrite(pinLedRojoP, HIGH);
+  }else if(contador >= (valorPot)) {
+    /*Serial.println(millis());
+    Serial.println(contador);
+    Serial.println(valorPot);*/
+    contador = 0;
+    delay(tiempoParpadeo);    
+    digitalWrite(pinLedVerde, LOW);
+    delay(tiempoParpadeo);    
+    digitalWrite(pinLedVerde, HIGH);
+    delay(tiempoParpadeo);    
+    digitalWrite(pinLedVerde, LOW);
+    delay(tiempoParpadeo);
+    digitalWrite(pinLedVerde, HIGH);
+    delay(tiempoParpadeo);    
+    digitalWrite(pinLedVerde, LOW);
+    delay(tiempoParpadeo);
+    digitalWrite(pinLedAmarillo, HIGH);
+    delay(tiempoLedAmarillo);
+    digitalWrite(pinLedAmarillo, LOW);
+    digitalWrite(pinLedRojoP, LOW);
+    digitalWrite(pinLedRojo, HIGH);
+    digitalWrite(pinLedVerdeP, HIGH);
+    delay(tiempoLedRojo); 
+    flag = true;  
+  }  
+}
+
+void apagarLeds() {
+  digitalWrite(pinLedVerde, LOW);
+  digitalWrite(pinLedAmarillo, LOW);
+  digitalWrite(pinLedRojo, LOW);
+  digitalWrite(pinLedVerdeP, LOW);
+  digitalWrite(pinLedRojoP, LOW);
+}
